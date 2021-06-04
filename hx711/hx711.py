eb298f1d03510b1126f9ce2ebfc0823f8c444661
James Morris <jemorris55@gmail.com>
2021-06-04 08:50:45 -0700

    added _pulse_sck_high and _write_channel_gain Added read of 24 bits and writing of channel gain Added functions to LoadCell to handle raw reading and 2s complement conversions

--- hx711/hx711.py
+++ hx711/hx711.py
@@ -112,6 +112,53 @@ class HX711:
                 # if not ready sleep for 10ms before next iteration
                 sleep(0.01)                
         return ready
+    
+    def _pulse_sck_high(self):
+        """
+        Pulse SCK pin high shortly
+        
+        Returns:
+            bool: True if pulse was shorter than 60 ms
+        """
+        
+        pulse_start = perf_counter()
+        GPIO.output(self._pd_sck, True)
+        GPIO.output(self._pd_sck, False)
+        pulse_end = perf_counter()
+        # check if pulse lasted 60ms or longer. If so, HX711 enters power down mode
+        if pulse_end - pulse_start >= 0.00006:  # check if the hx 711 did not turn off...
+            # if pd_sck pin is HIGH for 60 us and more than the HX 711 enters power down mode.
+            if self._debug_mode:
+                print(f'sck pulse lasted for longer than 60ms\nTime elapsed: {pulse_end - pulse_start}')
+            return False
+        return True
+    
+    def _write_channel_gain(self):
+        """
+        _write_channel_gain must be run after each 24-bit read
+        pulses SCK pin 1, 2, or 3 times based on channel configuration
+        
+        A, 128 : total pulses = 25 (24 read data, 1 extra to set dout back to high)
+        A, 64 : total pulses = 27 (24 read data, 3 extra to set dout back to high)
+        B, 32 : total pulses = 26 (24 read data, 2 extra to set dout back to high)
+        
+        Returns:
+            bool: True if pulsees were all successful
+        """
+        
+        # get number of pulses based on channel configuration
+        num_pulses = 2 # default 2 for channel B
+        if self._channel_select == 'A' and self._channel_A_gain == 128:
+            num_pulses = 1
+        elif self._channel_select == 'A' and self._channel_A_gain == 64:
+            num_pulses = 3
+        
+        # pulse num_pulses
+        for _ in range(num_pulses):
+            if not self._pulse_sck_high():
+                return False
+        return True
+            
         
         
     def _read(self):
@@ -132,6 +179,23 @@ class HX711:
                 print('_prepare_to_read() not ready after 20 iterations\n')
             return False
         
+        # read first 24 bits of data (the raw data bits)
+        load_cell: LoadCell
+        for load_cell in self._load_cells:
+            load_cell._init_raw_read()
+        for _ in range(24):
+            # pulse sck high to request each bit
+            if not self._pulse_sck_high():
+                return False
+            for load_cell in self._load_cells:
+                load_cell._read()
+        for load_cell in self._load_cells:
+            load_cell._finish_raw_read()
+                
+        # set channel after read
+        if not self._write_channel_gain():
+            return False
+        
         return True
             
 class LoadCell:
@@ -149,4 +213,42 @@ class LoadCell:
         self._scale_ratio = 1.
         self._last_raw_read = None
         self.raw_reads = []
+        self.reads = []
+        
+    def _init_raw_read(self):
+        # set raw read value to zero, so each bit can be shifted into this value
+        self._current_raw_read = 0
+    
+    def _read(self):
+        # left shift by one bit then bitwise OR with the new bit
+        self._current_raw_read = (self._current_raw_read << 1) | GPIO.input(self._dout_pin)
+        
+    def _finish_raw_read(self):
+        # append current raw read value to raw_reads list
+        self.raw_reads.append(self._current_raw_read)
+        # convert to signed value
+        self._current_signed_value = self.convert_to_signed_value(self._current_raw_read)
+        self.reads.append(self._current_signed_value)
+        if self._debug_mode:
+            # print 2's complement value and signed value
+            print(f'Binary value as received: {bin(self._current_raw_read)}\nSigned value: {self._current_signed_value}')
+            
+    def convert_to_signed_value(self, raw_value):
+        # convert to signed value after verifying value is valid
+        #check if data is valid by checking betwwen valid range: 0x800000 - 0x7fffff
+        if not (0x7fffff < raw_value < 0x800000):
+            if self._debug_mode:
+                print('Invalid raw value detected: {}\n'.format(raw_value))
+            return None  # return None because the data is invalid
+        # calculate int from 2's complement
+        # check if the sign bit is 1, indicating a negative number
+        if (raw_value & 0x800000):
+            signed_value = -((raw_value ^ 0xffffff) + 1)  # convert from 2's complement to negative int
+        else:  # else do not do anything the value is positive number
+            signed_value = raw_value
+        return signed_value
+    
+    def _init_set_of_reads(self):
+        self.raw_reads = []
+        self.reads = []
         
